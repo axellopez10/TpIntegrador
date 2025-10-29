60d03f55d8a3108b92ad7490b1119a4188ea85f6
axeljrrr <crazycrown1978@gmail.com>
2025-10-29 09:35:13 -0300

    cambios en imagenes
    agregue nuevas imagenes y reestructure las carpetas

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,10 +2,20 @@
 let imagenesArray = [
 
 
+    "./images/heladera.png",
+    "./images/lavarropa.png",
+    "./images/microondas.png",
+    "./images/secadora.png",
+    "./images/conjunto.png"
+                   
+    
+    
 
-    'https://img.freepik.com/psd-gratis/refrigerador-puerta-francesa-acero-inoxidable-elegante_632498-25861.jpg?semt=ais_hybrid&w=740&q=80',
-    'https://img.freepik.com/vector-gratis/horno-microondas-luz-interior-aislado-sobre-fondo-blanco-electrodomesticos-cocina_134830-658.jpg?semt=ais_hybrid&w=740&q=80',
-    'https://media.istockphoto.com/id/1463361334/es/foto/m%C3%A1quina-de-lavado.jpg?s=612x612&w=0&k=20&c=yWaU3hQ7x7XK74aLwq4GKr8U7iFs_JWFLI8jx56dzqw='
+
+
+    // 'https://img.freepik.com/psd-gratis/refrigerador-puerta-francesa-acero-inoxidable-elegante_632498-25861.jpg?semt=ais_hybrid&w=740&q=80',
+    // 'https://img.freepik.com/vector-gratis/horno-microondas-luz-interior-aislado-sobre-fondo-blanco-electrodomesticos-cocina_134830-658.jpg?semt=ais_hybrid&w=740&q=80',
+    // 'https://media.istockphoto.com/id/1463361334/es/foto/m%C3%A1quina-de-lavado.jpg?s=612x612&w=0&k=20&c=yWaU3hQ7x7XK74aLwq4GKr8U7iFs_JWFLI8jx56dzqw='
 
 ]
 
